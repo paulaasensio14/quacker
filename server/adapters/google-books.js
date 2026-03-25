825ceab496de5e7b72b7e5fb39fbcdf3148e863d
Paula Asensio Sánchez <paulaasensiodaw@gmail.com>
2026-03-25 12:30:59 +0100

    remove spanish-only restriction from google books search

--- server/adapters/google-books.js
+++ server/adapters/google-books.js
@@ -88,7 +88,6 @@ export async function searchGoogleBooks(query) {
 
   const data = await _googleBooksGet("/volumes", {
     q,
-    langRestrict: "es",
     maxResults: 20,
     printType: "books",
     orderBy: "relevance"
