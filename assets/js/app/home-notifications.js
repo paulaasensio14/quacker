6978ae548346b8659986a2e1d8814a7441147ddf
Paula Asensio Sánchez <paulaasensiodaw@gmail.com>
2026-04-10 17:32:40 +0200

    fix(ui): replace default notification icon to avoid confusion with action check

--- assets/js/app/home-notifications.js
+++ assets/js/app/home-notifications.js
@@ -72,11 +72,12 @@ const NotificationsUI = (() => {
       `;
     }
 
-    // default: check
+    // default: bell
     return `
       <svg width="18" height="18" viewBox="0 0 24 24" fill="none"
         stroke="currentColor" stroke-width="2.4" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
-        <path d="M5 12l4 4 10-10"></path>
+        <path d="M18 8a6 6 0 0 0-12 0c0 7-3 7-3 7h18s-3 0-3-7"></path>
+        <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
       </svg>
     `;
   }
