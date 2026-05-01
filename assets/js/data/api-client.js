ce522e32be68418bda226599e136fa9ce8cc371c
Paula Asensio Sánchez <paulaasensiodaw@gmail.com>
2026-05-01 23:20:23 +0200

    fix(explore): include books in weekly featured feed

--- assets/js/data/api-client.js
+++ assets/js/data/api-client.js
@@ -1050,6 +1050,7 @@ const ApiClient = (() => {
     const FEATURED_REQUESTS = [
       { type: "serie", limit: 5 },
       { type: "pelicula", limit: 5 },
+      { type: "book", limit: 2 },
       { type: "game", limit: 3 }
     ];
 
